69814ece0549ce059a07e847731a4b2f20f1624f
thegreat-vanderlinde <thegreat.vanderlinde@protonmail.com>
2022-01-03 11:45:45 +0100

    using standard anchor constraints

--- tests/create_mint_and_vault.ts
+++ tests/create_mint_and_vault.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { getMintInfo, getTokenAccount } from "@project-serum/common";
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { CreateMintAndVault } from "../target/types/create_mint_and_vault";
 import assert from "assert";
 
@@ -10,32 +10,45 @@ describe("create_mint_and_vault", () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.CreateMintAndVault as Program<CreateMintAndVault>;
+  console.log("program:" + program.programId);
+
+  async function findAssociatedTokenAddress(walletAddress: anchor.web3.PublicKey, tokenMintAddress: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> {
+    return (await anchor.web3.PublicKey.findProgramAddress([walletAddress.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMintAddress.toBuffer()], ASSOCIATED_TOKEN_PROGRAM_ID))[0];
+  }
 
   it("create_mint_and_vault", async () => {
     const decimals = 0;
     const amount = 1000;
 
     const mint = anchor.web3.Keypair.generate();
-    const vault = anchor.web3.Keypair.generate();
+    const accounts = {
+      authority: program.provider.wallet.publicKey,
+      mint: mint.publicKey,
+      vault: vault,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    };
 
-    await program.rpc.createMintAndVault(decimals, new anchor.BN(amount), {
-      accounts: {
-        authority: program.provider.wallet.publicKey,
-        mint: mint.publicKey,
-        vault: vault.publicKey,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      },
-      signers: [mint, vault],
+    console.log("accounts:");
+    Object.keys(accounts).forEach((k) => {
+      console.log(k + " = " + accounts[k]);
     });
-    // console.log("tx", tx);
+
+    console.log();
+    console.log("execute rpc");
+    const tx = await program.rpc.createMintAndVault(decimals, new anchor.BN(amount), {
+      accounts,
+      signers: [mint],
+    });
+    console.log("tx", tx);
 
     const mintInfo = await getMintInfo(program.provider, mint.publicKey);
     assert.equal(mintInfo.decimals, decimals);
     assert.equal(mintInfo.supply.toNumber(), amount);
 
-    const vaultAccountInfo = await getTokenAccount(program.provider, vault.publicKey);
+    const vaultAccountInfo = await getTokenAccount(program.provider, vault);
     assert.equal(vaultAccountInfo.amount.toNumber(), amount);
     assert.deepEqual(vaultAccountInfo.mint, mint.publicKey);
     assert.deepEqual(vaultAccountInfo.owner, program.provider.wallet.publicKey);
